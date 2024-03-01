8e6794b071fa3d90c1b40783f8bdb71332076161
Jemartri44 <jemartri@gmail.com>
2024-03-01 05:38:22 +0100

    Basic header and footer implemented

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -9,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router:Router) {  }
+
+  goToManual() {
+    console.debug("Redirecting to manual")
+    this.router.navigate(['/manual']);
+  }
+
+  goToAccount() {
+    console.debug("Redirecting to account")
+    this.router.navigate(['/account']);
+  }
+
+  goToLogout() {
+    console.debug("Logging out and redirecting to login")
+    this.router.navigate(['/logout']);
+  }
 }
