b460b37348fac021f54032a9885ea5cdeb21e8bd
Jemartri44 <jemartri@gmail.com>
2024-03-05 20:22:50 +0100

    Implemented navigation bar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { PatientListComponent } from './component/patient/patient-list/patient-list.component';
 import { authGuard } from './auth/guard/auth.guard';
+import { PatientInfoComponent } from './component/patient/patient-info/patient-info.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent, canActivate: [authGuard] },
     { path: 'register', component: RegisterComponent, canActivate: [authGuard] },
     { path: 'pacientes', component: PatientListComponent, canActivate: [authGuard] },
+    { path: 'pacientes/:id', component: PatientInfoComponent, canActivate: [authGuard] },
     //{ path: 'manual', component: ManualComponent, canActivate: [authGuard] },
     { path: '', redirectTo: '/pacientes', pathMatch: 'full'},
     { path: '**', redirectTo: '/pacientes', pathMatch: 'full'}
