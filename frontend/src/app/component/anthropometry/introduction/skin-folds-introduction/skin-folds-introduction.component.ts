0119012b34ca3b442d27a52a47d685e4824ddccf
Jemartri44 <jemartri@gmail.com>
2025-01-21 20:50:54 +0100

    Fixed kid's skin folds description and formulas

--- frontend/src/app/component/anthropometry/introduction/skin-folds-introduction/skin-folds-introduction.component.ts
+++ frontend/src/app/component/anthropometry/introduction/skin-folds-introduction/skin-folds-introduction.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { AnthropometryData } from '../../../../model/anthropometry/anthropometry-data';
+import { SkinFolds } from '../../../../model/anthropometry/skin-folds';
 
 @Component({
   selector: 'app-skin-folds-introduction',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './skin-folds-introduction.component.html',
   styleUrl: './skin-folds-introduction.component.css'
 })
 export class SkinFoldsIntroductionComponent {
+  @Input() data: AnthropometryData | null | undefined;
+  kid: boolean = false;
 
+  ngOnInit() {
+    if (this.data) {
+      let skinFolds = this.data as SkinFolds;
+      if(skinFolds.age == undefined) {
+        throw new Error ("No se pudo recuperar la información sobre el paciente");
+      }
+      if(skinFolds.age < 18) {
+        this.kid = true;
+      } else { 
+        this.kid = false;
+      }
+    }
+  }
 }
